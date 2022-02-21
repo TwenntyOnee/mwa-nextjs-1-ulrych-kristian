58498690c9b61e0633380c021a326c1f65fcc12c
TwenntyOnee <kristian.ulrych@gmail.com>
2022-02-21 16:53:30 +0100

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      'Basic':['Alfa Slab One'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
